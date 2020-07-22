efdc19cb3050da8fe4fbff4a09a79ffaa1f17492
jhamadhav <jhamadhav28@gmail.com>
2020-07-22 21:07:12 +0530

    minor patch

--- tooltip/scripts/tooltip.js
+++ tooltip/scripts/tooltip.js
@@ -1,4 +1,26 @@
+// some global variables
+let W, H, x, y;
+
 window.onload = () => {
+
+    // get window inner height and width
+    H = window.innerHeight;
+    W = window.innerWidth;
+    // console.log(`H : ${H}  W : ${W}`);
+
+    window.addEventListener("mousemove", (e) => {
+        // console.log(e);
+        x = e.clientX;
+        y = e.clientY;
+        // console.log(`x : ${x}  y : ${y}`);
+
+        // for this instance alone place tooltip every time mouse move
+        place_tool();
+    });
+}
+
+// function to place tooltip
+const place_tool = () => {
     //get the tool tip
     let tt = document.getElementsByClassName("tooltip")[0];
 
@@ -11,42 +33,30 @@ window.onload = () => {
     let tool_w = tool_shape.width;
     // console.log(`h : ${tool_h}  w : ${tool_w}`);
 
-    // get window inner height and width
-    let H = window.innerHeight;
-    let W = window.innerWidth;
-    // console.log(`H : ${H}  W : ${W}`);
+    let tt_x = 0, tt_y = 0;
+    // conditions i.e the tool tip should show all the way 
 
-    window.addEventListener("mousemove", (e) => {
-        // console.log(e);
-        let x = e.clientX;
-        let y = e.clientY;
-        // console.log(`x : ${x}  y : ${y}`);
-
-        let tt_x = 0, tt_y = 0;
-        // conditions i.e the tool tip should show all the way 
-
-        // for X co-ordinate
-        if (W - x >= tool_w) {
-            tt_x = x;
+    // for X co-ordinate
+    if (W - x >= tool_w) {
+        tt_x = x;
+    } else {
+        if (x >= tool_w) {
+            tt_x = x - tool_w;
         } else {
-            if (x >= tool_w) {
-                tt_x = x - tool_w;
-            } else {
-                tt_x = 0;
-            }
+            tt_x = W - tool_w;
         }
-        // for Y co-ordinate
-        if (H - y > tool_h) {
-            tt_y = y;
+    }
+    // for Y co-ordinate
+    if (H - y > tool_h) {
+        tt_y = y;
+    } else {
+        if (y >= tool_h) {
+            tt_y = y - tool_h;
         } else {
-            if (y >= tool_h) {
-                tt_y = y - tool_h;
-            } else {
-                tt_y = 0;
-            }
+            tt_y = 0;
         }
+    }
 
-        tt.style.transform = `translate(${tt_x}px,${tt_y}px)`;
-    });
+    tt.style.transform = `translate(${tt_x}px,${tt_y}px)`;
 
 }
